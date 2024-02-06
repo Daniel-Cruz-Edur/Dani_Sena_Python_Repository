bc2857ff53544b8c1819ec7e0a551b55746bd73c
Daniel <daniel_ecruz@soy.sena.edu.co>
2024-02-06 08:51:04 -0500

    Ejercicio_Loop_While

--- Bucles.py
+++ Bucles.py
@@ -1,1 +1,24 @@
-print("EL precio de entrada es: ");
+import random
+
+Secreto = random.randint(1, 10);
+    
+Num_User = int(input("Ingresa un número entre 1 y 10: "));
+
+
+while Num_User != Secreto:
+
+    if Num_User < Secreto:
+        print("El número secreto es mayor.");
+        print("Intenta nuevamente. ");
+
+    elif Num_User > Secreto:
+        print("El número secreto es menor.");
+        print("Intenta nuevamente. ");
+        
+    Num_User = int(input("Ingresa un número entre 1 y 10: "));
+
+input("Felicdades, haz adivinado el número secreto. ");
+
+
+
+    
